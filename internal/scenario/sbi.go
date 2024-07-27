c5c8850c1c753cae10a7fc3bcbd2322f90d38179
azuki774s <azuki774s@gmail.com>
2024-07-27 22:34:32 +0900

    add extract SBI and output files

--- internal/scenario/sbi.go
+++ internal/scenario/sbi.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"myscrapers/internal/csv"
 	"os"
 	"path/filepath"
 	"time"
@@ -16,16 +17,15 @@ import (
 )
 
 const sbiLoginURL = "https://site1.sbisec.co.jp/ETGate/"
+const portfolioURL = "https://site1.sbisec.co.jp/ETGate/?_ControlID=WPLETpfR001Control&_PageID=DefaultPID&_DataStoreID=DSWPLETpfR001Control&_ActionID=DefaultAID&getFlg=on"
+
+const portfolioFieldSize = 12
 
 type ScenarioSBI struct {
 	common  ScenarioCommon
 	browser *rod.Browser
 	user    string
 	pass    string
-	// result
-
-	Headers []string
-	Bodies  [][]string
 }
 
 func NewScenarioSBI() (*ScenarioSBI, error) {
@@ -69,12 +69,7 @@ func (s *ScenarioSBI) getBrowser(ctx context.Context) error {
 	return nil
 }
 
-func (s *ScenarioSBI) Start(ctx context.Context) error {
-	if err := s.getBrowser(ctx); err != nil {
-		return err
-	}
-	defer s.browser.Close()
-
+func (s *ScenarioSBI) login(ctx context.Context) error {
 	slog.Info("load loginpage start")
 	page := s.browser.MustPage(sbiLoginURL).MustWaitStable()
 	slog.Info("load loginpage complete")
@@ -86,7 +81,6 @@ func (s *ScenarioSBI) Start(ctx context.Context) error {
 		return err
 	}
 	loginField.MustInput(s.user)
-	fmt.Println(loginField.MustText())
 
 	// パスワード入力
 	passField, err := page.Timeout(10 * time.Second).Element(`[name="user_password"]`)
@@ -94,9 +88,17 @@ func (s *ScenarioSBI) Start(ctx context.Context) error {
 		return err
 	}
 	passField.MustInput(s.pass).MustType(input.Enter) // Enter キーでそのまま送信
-	page.MustWaitStable()
+	if err := page.WaitStable(10 * time.Second); err != nil {
+		return err
+	}
+	slog.Info("login sequence complete")
+	return nil
+}
 
-	page = s.browser.MustPage("https://site1.sbisec.co.jp/ETGate/?_ControlID=WPLETpfR001Control&_PageID=DefaultPID&_DataStoreID=DSWPLETpfR001Control&_ActionID=DefaultAID&getFlg=on").MustWaitStable()
+func (s *ScenarioSBI) getPortfolio(ctx context.Context) error {
+	// ポートフォリオページに移動
+	slog.Info("move Portfolio page")
+	page := s.browser.Timeout(30 * time.Second).MustPage(portfolioURL).MustWaitStable()
 	img, err := page.Screenshot(true, &proto.PageCaptureScreenshot{
 		Format: proto.PageCaptureScreenshotFormatPng,
 		Clip: &proto.PageViewport{
@@ -116,5 +118,88 @@ func (s *ScenarioSBI) Start(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
+	tables1 := page.MustElements("tbody") // table1: tbody を全部抜き出し
+	var tables2 []rod.Elements            // table2: ポートフォリオテーブルのみ抜き出し
+	for _, t := range tables1 {
+		val, err := t.Elements("td")
+		if err != nil {
+			continue
+		}
+		if len(val) == 0 {
+			continue
+		}
+		if val[0].MustText() != "取引" {
+			// 一番左上の要素が「取引」でないものは目的のテーブルでないので除外
+			continue
+		}
+		slog.Info("datected Portfolio table")
+		tables2 = append(tables2, val)
+	}
+
+	// 1テーブルごとに処理
+	for i, table := range tables2 {
+		elNum := len(table)
+		rowNum := elNum / portfolioFieldSize
+		slog.Info("extract table", "table", i, "elementNum", elNum, "rowNum", rowNum)
+		// 取引,ファンド名,買付日,数量,取得単価,現在値,前日比,前日比（％）,損益,損益（％）,評価額,編集
+		var headers []string
+		var bodies [][]string
+
+		headers = []string{
+			table[0].MustText(),
+			table[1].MustText(),
+			table[2].MustText(),
+			table[3].MustText(),
+			table[4].MustText(),
+			table[5].MustText(),
+			table[6].MustText(),
+			table[7].MustText(),
+			table[8].MustText(),
+			table[9].MustText(),
+			table[10].MustText(),
+			table[11].MustText(),
+		}
+		for i := 1; i < rowNum; i++ { // Body (not Header)
+			tmpBody := []string{
+				table[i*portfolioFieldSize+0].MustText(),
+				table[i*portfolioFieldSize+1].MustText(),
+				table[i*portfolioFieldSize+2].MustText(),
+				table[i*portfolioFieldSize+3].MustText(),
+				table[i*portfolioFieldSize+4].MustText(),
+				table[i*portfolioFieldSize+5].MustText(),
+				table[i*portfolioFieldSize+6].MustText(),
+				table[i*portfolioFieldSize+7].MustText(),
+				table[i*portfolioFieldSize+8].MustText(),
+				table[i*portfolioFieldSize+9].MustText(),
+				table[i*portfolioFieldSize+10].MustText(),
+				table[i*portfolioFieldSize+11].MustText(),
+			}
+			bodies = append(bodies, tmpBody)
+		}
+		fmt.Println("show header")
+		fmt.Println(headers)
+		fmt.Println("show body")
+		fmt.Println(bodies)
+		csv.WriteFile(fmt.Sprintf("./portfolio_%d.csv", i+1), headers, bodies) // filename: 0-indexed -> 1-indexed
+	}
+
+	return nil
+}
+
+func (s *ScenarioSBI) Start(ctx context.Context) error {
+	if err := s.getBrowser(ctx); err != nil {
+		return err
+	}
+	defer s.browser.Close()
+
+	if err := s.login(ctx); err != nil {
+		return err
+	}
+
+	if err := s.getPortfolio(ctx); err != nil {
+		return err
+	}
+
 	return nil
 }
