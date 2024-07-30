23d550c2354bd04c43b696697205aeabb2d98685
azuki774s <azuki774s@gmail.com>
2024-07-31 02:47:10 +0900

    wip: success login sequence

--- README.md
+++ README.md
@@ -8,3 +8,17 @@
     - outputDir オプションがあった場合は、${outputDir}/YYYYMMDD_x.csv
 - s3ストレージにアップロードへの機能がある。
     - 環境変数 `BUCKET_NAME` があった場合、取得したデータを `s3://${BUCKET_NAME}/${REMOTE_DIR}/YYYYMMDD/` に保存。
+
+## Quick start (binary)
+
+```
+docker run --rm -p 7327:7327 ghcr.io/go-rod/rod:v0.116.2
+```
+
+```
+user=<your id> \
+pass=<your pass> \
+outputDir="." \
+wsAddr="localhost:7327" `
+build/bin/myscrapers download moneyforward
+```
--- internal/scenario/moneyforward.go
+++ internal/scenario/moneyforward.go
@@ -2,15 +2,17 @@ package scenario
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/input"
 	"github.com/go-rod/rod/lib/launcher"
 )
+
+const mfCfURL = "https://moneyforward.com/cf" // also for login page without account_selector
 
 type ScenarioMoneyForward struct {
 	common    ScenarioCommon
@@ -59,18 +61,54 @@ func (s *ScenarioMoneyForward) getBrowser(ctx context.Context) error {
 	l.Set("disable-gpu").Delete("disable-gpu")
 
 	// Launch with headful mode
-	l.Headless(true).XVFB("--server-num=5", "--server-args=-screen 0 1600x900x16")
+	l.Headless(false).XVFB("--server-num=5", "--server-args=-screen 0 1600x900x16")
 
 	s.browser = rod.New().Client(l.MustClient()).MustConnect()
 	return nil
 }
+
+func (s *ScenarioMoneyForward) login(ctx context.Context) error {
+	slog.Info("load loginpage start")
+	page := s.browser.Timeout(60 * time.Second).MustPage(mfCfURL).MustWaitStable()
+	slog.Info("load loginpage complete")
+
+	slog.Info("login information input")
+	// ID入力
+	// loginField, err := page.Element(`[placeholder="example@moneyforward.com"]`)
+	loginField, err := page.ElementX("/html/body/main/div/div/div[2]/div/section/div/form/div/div/input")
+	if err != nil {
+		return err
+	}
+	loginField.MustInput(s.user).MustType(input.Enter)
+
+	// ここでは画面遷移しないので WaitStableしない
+	time.Sleep(10 * time.Second) // 固定値スリープで代用
+
+	// パスワード入力
+	passField, err := page.ElementX("/html/body/main/div/div/div[2]/div/section/div/form/div/div[2]/input")
+	// passField, err := page.Element(`[type="password"]`)
+	if err != nil {
+		return err
+	}
+
+	passField.MustInput(s.pass).MustType(input.Enter)
+
+	time.Sleep(10 * time.Second) // 固定値スリープで代用
+	slog.Info("login sequence complete")
+
+	return nil
+}
+
 func (s *ScenarioMoneyForward) Start(ctx context.Context) (err error) {
 	if err := s.getBrowser(ctx); err != nil {
 		return err
 	}
 	defer s.browser.Close()
 
-	fmt.Println("TODO")
+	if err := s.login(ctx); err != nil {
+		slog.Error("login failed")
+		return err
+	}
 
 	return nil
 }
