9471fcafaac7069a82ce9bb54eaca56d24d32e96
azuki774s <azuki774s@gmail.com>
2024-07-30 23:01:31 +0900

    use const value

--- internal/csv/model.go
+++ internal/csv/model.go
@@ -5,11 +5,11 @@ import "fmt"
 const cfFieldSize = 10
 
 func ValidateCF(header []string, bodies [][]string) error {
-	if len(header) != 10 {
+	if len(header) != cfFieldSize {
 		return fmt.Errorf("invalid field size: header")
 	}
 	for i, b := range bodies {
-		if len(b) != 10 {
+		if len(b) != cfFieldSize {
 			return fmt.Errorf("invalid field size: bodies #%d", i+1)
 		}
 	}
