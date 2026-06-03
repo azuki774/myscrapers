fce8409f40a31d9c72e6df0c40af030a64456dd8
azuki774 <azuki774s@gmail.com>
2026-06-03 16:15:39 +0000

    feat: add myscraper github smoke test

--- myscraper/internal/cli/run.go
+++ myscraper/internal/cli/run.go
@@ -1,9 +1,34 @@
 package cli
 
-import "io"
+import (
+	"context"
+	"fmt"
+	"io"
 
-func Run(stdout, stderr io.Writer) int {
-	_ = stdout
-	_ = stderr
+	"github.com/azuki774/myscrapers/myscraper/internal/scrape"
+)
+
+type Runner interface {
+	Run(ctx context.Context, req scrape.Request) (scrape.Result, error)
+}
+
+func Run(args []string, stdout, stderr io.Writer, runner Runner) int {
+	opts, err := ParseArgs(args)
+	if err != nil {
+		fmt.Fprintln(stderr, err)
+		return 2
+	}
+
+	result, err := runner.Run(context.Background(), scrape.Request{
+		URL:        opts.URL,
+		OutputPath: opts.OutputPath,
+		Headless:   opts.Headless,
+	})
+	if err != nil {
+		fmt.Fprintln(stderr, err)
+		return 1
+	}
+
+	fmt.Fprintf(stdout, "saved %s (%s)\n", result.OutputPath, result.Title)
 	return 0
 }
